540ad2cb8298b97bd3ec5d2ff4e41562d735f39b
Stas <stas@Stass-MacBook-Air.local>
2019-05-19 10:41:46 +0200

    Fix Typos

--- patveiwer-make-package.r
+++ patveiwer-make-package.r
@@ -1,4 +1,4 @@
-## Creates OrbisR package
+## Creates PatStatr package
 
 ## --------------------------------------------------------------------------------
 ## Load or Install Packages
@@ -19,7 +19,7 @@ for(pkg in c('pbapply'
 
 ## Making a package
 ## --------------------------------------------------------------------------------
-setwd("~/org/data/patstat/patstatr")
+## setwd("~/org/data/patstat/patstatr")
 
 ## Updates package info
 person("Stas", "Vlasov", 
@@ -32,12 +32,18 @@ person("Stas", "Vlasov",
 
 ## Assume that it runs from "harmonizer" directory
 list(Title  = "Tools for working with raw PATSTAT database"
-   , Date = "2018-11-27"
+   , Date = "2018-12-21"
    , License = "MIT License"
    , Description = "Set of functions that help to prepare, to load into R session and to search PATSTAT data"
+   , Depends = "R (>= 3.4.1)"
    , References = "patstat-biblio-2017-autumn-txt") %>% 
     {setup(rstudio = FALSE
          , description = .)}
+
+
+
+## usethis::create_package()
+## usethis::use_description()
 
 
 ## Update name spaces and documentation for functions
